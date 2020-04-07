c8170e6c14d58cfb241b216cac6ba293e0aa6b47
Amin0Mohamed <aminelcaptain20@gmail.com>
2020-04-07 02:37:54 +0200

    finish nav

--- js/style.js
+++ js/style.js
@@ -66,5 +66,58 @@ function getResolution() {
     }
 }
 
+//*****************navbar-toggler****************************
+function toggle(e) {
+    var ele = document.querySelector('.navbar-toggler');
+    var nav = document.querySelector('nav');
+    var one = document.querySelector('.navbar-toggler span:nth-child(1)');
+    var two = document.querySelector('.navbar-toggler span:nth-child(2)');
+    var three = document.querySelector('.navbar-toggler span:nth-child(3)');
+    var nav_menu = document.querySelector('#navbarSupportedContent');
+    var small_btn = document.querySelector('header nav .question_btn');
+    if (ele.classList.contains("ss") ){
+        ele.classList.remove('ss');
+        nav.classList.add('nav_small');
+        one.classList.add('one');
+        two.classList.add('two');
+        three.classList.add('three');
+        nav_menu.classList.add('nav_menu');
+        small_btn.classList.add('small_question_btn');
+    }else{
+        ele.classList.add('ss');
+        ele.classList.remove('s');
+        nav.classList.remove('nav_small');
+        one.classList.remove('one');
+        two.classList.remove('two');
+        three.classList.remove('three');
+        nav_menu.classList.remove('nav_menu');
+        small_btn.classList.remove('small_question_btn');
+    }
+}
+
+setInterval(getnav,500);
+function getnav() {
+    var nav = document.querySelector('nav');
+    var ele = document.querySelector('.navbar-toggler');
+    var nav_menu = document.querySelector('#navbarSupportedContent');
+    var small_btn = document.querySelector('header nav .question_btn');
+
+    if (window.innerWidth >= 992){
+        nav.classList.remove('nav_small');
+        nav_menu.classList.remove('nav_menu');
+        small_btn.classList.remove('small_question_btn');
+    }else {
+        if (ele.classList.contains("ss") ){
+            nav.classList.remove('nav_small');
+        }else {
+            ele.classList.add('s');
+            ele.classList.remove('ss');
+            nav.classList.add('nav_small');
+            nav_menu.classList.add('nav_menu');
+            small_btn.classList.add('small_question_btn');
+        }
+    }
+}
+
 
 
